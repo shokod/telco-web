b429a4fea24ddc7743f5420e8f64453d91f5c7cf
shokod <delvinshoko@gmail.com>
2025-03-10 17:47:27 +0200

    Feat! Implemen products page

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "img.clerk.com",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
     ],
   },
 };
